8c9bc4797fe6d442048883220bf13294a027516d
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2024-09-17 16:33:11 -0400

    lab4_first_question

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { WapperList } from './components/WrapperList';
+import { List } from './components/List';
+import { Button } from './components/Button'
+
+function App() {
+
+    /* const buttonText="count :";*/
+
+  return (
+    <>
+      <WapperList>
+        <List/>
+      </WapperList>
+      <Button/>
+      {/* <h1>My name is Dietmar Alex Apaza</h1>
+      <Button text={buttonText}/> */}
+    </>
+  )
+}
+export default App
